1f83010ec4fff9040ac4ab75571ad1d4e15734c6
Xiphoray <351014842@qq.com>
2017-11-07 21:05:53 +0800

    fdsa

--- ant.py
+++ ant.py
@@ -6,18 +6,21 @@ from pylab import *
 import random
 import sys
 import G2D
+import copy
 
-NNUM = 5
-G = matrix([[0,0,0,0,0],
-				[0,1,0,0,0],
-				[0,0,0,0,0],
-				[0,0,0,0,0],
-				[0,0,0,0,0]])
+NNUM = 7
+G = matrix([[0,1,0,0,0,0,0],
+						[0,1,0,0,0,1,1],
+						[0,0,1,0,0,1,1],
+						[0,1,0,1,0,1,1],
+						[0,0,0,1,0,1,0],
+						[1,0,0,0,0,0,1],
+						[1,1,1,0,0,0,0]])
 MM = G.shape[0]   #G 地形图为01矩阵，如果为1表示障碍物
 Tau = ones((MM*MM,MM*MM)) # Tau 初始信息素矩阵（认为前面的觅食活动中有残留的信息素）
 Tau = 8.*Tau
 K = 100   #K 迭代次数（指蚂蚁出动多少波）
-M = 5   #M 蚂蚁个数（每一波蚂蚁有多少个）
+M = 50   #M 蚂蚁个数（每一波蚂蚁有多少个）
 S = 0   #S 起始点（最短路径的起始点）
 E = MM*MM - 1   #E 终止点（最短路径的目的点）
 Alpha = 1    # Alpha 表征信息素重要程度的参数
@@ -30,8 +33,6 @@ minl = 0
 
 D = G2D.G2D(G)
 
-TEST = D
-print('333333333')
 N = D.shape[0] 
 a = 1
 Ex = a * (mod(E,MM - 1) - 0.5)   #终止点横坐标
@@ -56,7 +57,7 @@ ROUTES = []
 PL = zeros((K,M))
 k = 0
 while(k != K):
-	m = 1
+	m = 0
 	while(m != M):
 		W = S
 		Path = []
@@ -64,11 +65,7 @@ while(k != K):
 		PLkm = 0
 		TABUkm = ones(N)
 		TABUkm[S] = 0
-		DD = D
-		#print(D)     #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		#print(G[1,1])
-		#print('1111111')
-		#print(TEST)
+		DD = copy.deepcopy(D)
 		DW = DD[W,:]
 		DW1 = nonzero(DW)
 		j = 0
@@ -119,9 +116,7 @@ while(k != K):
 				j += 1
 			LJD = nonzero(DW)
 			Len_LJD = len(LJD[0])
-			#print(W)	
-			#print(TEST[1][0])
-			#print('-----')
+			
 		
 		ROUTES.append(Path)
 		if (Path[-1] == E):
@@ -132,11 +127,49 @@ while(k != K):
 				minkl = PLkm
 		else:
 			PL[k][m] = 0
-			
-		print(TEST)
-		print('1111111')			
 		m += 1
-	break	
+	Delta_Tau = zeros((N,N))
+	m = 0
+	while(m != M):
+		if(PL[k][m] != 0):
+			ROUT = copy.deepcopy(ROUTES[k*M + m])
+			TS = len(ROUT) - 1
+			PL_km = PL[k][m]
+			s = 0
+			while(s != TS):
+				x = ROUT[s]
+				y = ROUT[s + 1]
+				Delta_Tau[x][y] = Delta_Tau[x][y] + Q/PL_km
+				Delta_Tau[y][x] = Delta_Tau[y][x] + Q/PL_km
+				s += 1
+		m += 1
+	Tau = (1 - Rho) * Tau + Delta_Tau
 	k += 1
-		
-
+print(ROUTES[mink*M + minl])		
+t = 0
+xian = ''
+while(t != NNUM):
+	xian += '---'
+	t += 1
+Pic = [xian]
+i = 0
+while(i != NNUM):
+	j = 0
+	Pstr = '|'
+	while(j != NNUM):
+		P = False
+		for q in ROUTES[mink*M + minl]:
+			if((i*NNUM + j) == q ):
+				P = True
+		if (P):
+			Pstr += ' *|'
+		elif(G[i,j] == 0):
+			Pstr += '  |'
+		else:
+			Pstr += '▇|'
+		j += 1	
+	Pic.append(Pstr)
+	Pic.append(xian)
+	i += 1
+for x in Pic:
+	print(x)
